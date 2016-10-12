f96fa6dc71c7f742d934ef8226eb72b9d5cc8705
Shoma Nishitateno <shoma416@gmail.com>
2016-10-12 16:16:02 +0900

    Dynamic inventory.

---
+++ hosts/inventory.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+usage() {
+  echo "Usage: inventory.sh --list | --host hostname"
+  exit 1
+}
+
+
+case $1 in
+  "--list")
+    IP=`virsh net-dhcp-leases default | grep localhost | awk -F' ' '{print $5}' | cut -d '/' -f 1`
+    echo {\"runner\":{\"hosts\":[\"$IP\"]}}
+    ;;
+  "--host")
+    echo '{"ansible_ssh_user":"root","ansible_ssh_pass":"password"}'
+    ;;
+  *)
+    usage
+    ;;
+esac
